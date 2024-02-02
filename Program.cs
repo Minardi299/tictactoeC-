87a265227d0bd9fc45e93dd27e4687b221120348
Minardi299 <phu.ngo@outlook.com>
2024-02-01 22:13:14 -0500

    complete program but still have bug with out of bound to fix

--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
-﻿namespace TicTacToe
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace TicTacToe
 {
 
     public class TicTacToeGrid
     {
-        public char[,] grid;    
+        public char[,] grid;   
+        public int turn; 
         public TicTacToeGrid(int size)
         {
             this.grid = new char[size,size];
@@ -14,22 +18,30 @@
                    grid[i,j]=' ';
                 }
             }
+            this.turn = 0;
           
         }
 
         public void PlaceCharacter(char x,int row, int column)
         {
-            row = row - 1;
-            column = column - 1;
-            while (row > this.grid.GetLength(0) && column > this.grid.GetLength(1))
+            row--;
+            column--;
+            while (row >= this.grid.GetLength(0) || column >= this.grid.GetLength(1))
             {
                 Console.WriteLine("Re Enter row: ");
                 row = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine("Re Enter column: ");
                 column = Convert.ToInt32(Console.ReadLine());
             }
+            // while (x != 'X' || x != 'O' )
+            // {
+            //     Console.WriteLine("Re Enter  X or O :");
+            //     ConsoleKeyInfo keyInfo = Console.ReadKey();
+            //     x = keyInfo.KeyChar ;
+            //     char.ToUpper(x);
+            // }
             this.grid[row,column] = x;
-            
+            this.turn++;
         }
 
         public void PrintGrid()
@@ -40,26 +52,118 @@
                 {
                     Console.Write("|"+grid[i,j]);
                 }
+                Console.Write("|");
                 Console.WriteLine();
             }
         }
         
-        private String CheckRows(){
-            return "The winner";
+        private char CheckRows(){
+            for (int i = 0; i < this.grid.GetLength(0); i++)
+            {
+                char symbol = grid[i,0];
+                bool match = true;
+                for  ( int j = 0; j < this.grid.GetLength(1); j++)
+                {
+                    
+                    if (this.grid[i,j] != symbol )
+                    {
+                        match = false;
+                        break;
+                    }
+                }
+                if(match)
+                {
+                    return symbol;
+                }
+            }
+            return ' ';
         }
-        private String CheckColumn(){
-            return "The winner";
+        private char CheckColumn(){
+            for (int i = 0; i < this.grid.GetLength(1); i++)
+            {
+                char symbol = this.grid[0,i];
+                bool match = true;
+                for  ( int j = 0; j < this.grid.GetLength(0); j++)
+                {
+                    
+                    if (this.grid[j,i] != symbol )
+                    {
+                        match = false;
+                        break;
+                    }
+                }
+                if(match)
+                {
+                    return symbol;
+                }
+            }
+            return ' ';
         }
-        private String CheckDiagonals(){
-            return "The winner";
+        private char CheckDiagonals(){
+            
+            char mainDiagonal = this.grid [0,0];
+            bool match = true;
+            for ( int i = 0; i < this.grid.GetLength(1); i++)
+            {
+                if ( this.grid[i,i] !=  mainDiagonal )
+                {
+                    match = false;
+                    break;
+                }
+            }
+            if(match)
+            {
+                return mainDiagonal;
+            }
+
+            char antiDiagonal = this.grid[0,this.grid.GetLength(0)-1];
+            bool antiMatch = true;
+            for ( int i = 0; i < this.grid.GetLength(0); i++)
+            {
+                if ( this.grid[i,this.grid.GetLength(0)-1-i] != antiDiagonal)
+                {
+                    antiMatch = false;
+                    break;
+                }
+            }
+            if(antiMatch)
+            {
+                return antiDiagonal;
+            }
+            return ' ';
         }
+
 
         public String CheckGrid(){
-            CheckDiagonals();
-            CheckRows();
-            CheckColumn();
+            char diagonal = CheckDiagonals();
+            if (diagonal != ' ')
+            {
+                return diagonal + " wins";
+            }
+            char row = CheckRows();
+            if (row != ' ')
+            {
+                return row + " wins";
+            }
+            char column = CheckColumn();
+            if (column != ' ')
+            {
+                return column + " wins";
+            }
 
-            return "The winner";
+            if (IsGridFull())
+            {
+                return "Tie";
+            }
+            return " ";
+        }
+        public bool IsGridFull()
+        {
+            if (this.turn >= this.grid.GetLength(0) * this.grid.GetLength(0))
+            {
+                return true;
+            }
+            return false;
         }
     }
 
@@ -67,14 +171,41 @@
     {
         static void Main(string[] args)
         {
-            TicTacToeGrid grid = new TicTacToeGrid(4);
-            Console.WriteLine("the main method");
-            grid.PlaceCharacter('H',4,1);
-            grid.PlaceCharacter('G',1,4);
-            grid.PrintGrid();
+            Console.WriteLine("Enter size of grid");
+            int size = Convert.ToInt32(Console.ReadLine());
+
+            TicTacToeGrid grid = new TicTacToeGrid(size);
+            while(grid.CheckGrid() == " ")
+            {
+                Console.WriteLine("Enter which row to play");
+                int row = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter which column to play");
+                int column = Convert.ToInt32(Console.ReadLine());
+                char character = ' ';
+                if(IsEven(grid.turn))
+                {
+                    character = 'X';
+                }
+                else{
+                    character = 'O';
+                }
+                grid.PlaceCharacter(character,row,column);
+                grid.PrintGrid();
+
+            }
+
+            Console.WriteLine(grid.CheckGrid());
+
+
+            
+
             // String winner = grid.CheckGrid();
             
 
         }
+        static bool IsEven(int number)
+    {
+        return number % 2 == 0;
+    }
     }
 }
